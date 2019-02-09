c0cea79cb2cd0c267a29395847832c3e5191a7ab
Kaleb Kougl <kalebkougl@gmail.com>
2019-02-09 09:19:19 -0600

    adding put method

--- restful_api.py
+++ restful_api.py
@@ -34,6 +34,16 @@ class Item(Resource):
         items = list(filter(lambda x: x["name"] != name, items))
         return {"message": "{} was deleted".format(name)}
 
+    def put(self, name):
+        data = request.json()
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        if item is None:
+            item = {"name": name, "price": data["price"]}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 
 class Items(Resource):
     def get(self):
